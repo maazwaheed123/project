f88ed093026711321ee65d519ba3a88bc3dcdc39
maazwaheed <mmwaheed@master-works.sa>
2026-02-28 16:43:35 +0530

    first-commit

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/api", (req, res) => {
   res.json({
-    message: "Hello from backend",
+    message: "Hello from backend 1234532",
     instance: process.env.INSTANCE_NAME
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on ${PORT} test1111`);
 });
