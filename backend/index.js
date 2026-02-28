2c80ba9c6b90c441749ac1f7f4cb9795a7edb9cd
maazwaheed <mmwaheed@master-works.sa>
2026-02-28 21:28:07 +0530

    fix docker deployment path

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/api", (req, res) => {
   res.json({
-    message: "Hello from backend 1234532",
+    message: "Hello from backend 1234532111111111111111",
     instance: process.env.INSTANCE_NAME
   });
 });
